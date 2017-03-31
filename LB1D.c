3eb37d5e12c5e20d2c65361f6b9ccefc10825cf1
Sebastian <cebasfu93@gmail.com>
2017-03-31 15:55:14 -0500

    Mallocs en main y no en funciones

--- LB1D.c
+++ LB1D.c
@@ -10,19 +10,22 @@
 #define Nv 1024
 #define Nu 1024
 #define Nw 1024
-#define L 2.0 //1.0 //2.0
-#define L_min -1.0// -0.5 //-1.0
+
+#define L 2.0 //1.0
+#define L_min -1.0 // -0.5
 #define V 2.0
 #define V_min -1.0
+
 #define pi 3.141592654
 #define G 6.67408E-11
 #define FLOAT double
-#define T 30
-#define skip 3
+
+#define T 5
+#define skip 1
 #define deltat 0.1
 
-FLOAT delx=L/(Nx-1);
-FLOAT delv=V/(Nv-1);
+FLOAT delx=L/(Nx);
+FLOAT delv=V/(Nv);
 FLOAT dely=L/(Ny);
 FLOAT delu=V/(Nu);
 FLOAT delz=L/(Nz);
@@ -31,14 +34,13 @@ FLOAT L_max = L_min+L;
 FLOAT V_max = V_min+V;
 int i,j,k;
 
-FLOAT gauss(FLOAT pos, FLOAT vel, FLOAT amp, FLOAT sigma);
-FLOAT jeans(FLOAT pos, FLOAT vel, FLOAT rho, FLOAT amp, FLOAT sig, int n);
-FLOAT * densidad(FLOAT *fase);
-FLOAT * potential(FLOAT *rho);
-FLOAT * potfourier(FLOAT *rho);
-FLOAT * potfourier_real(FLOAT *rho);
-FLOAT * acceleration(FLOAT *Va);
-FLOAT * update(FLOAT * fase, FLOAT * azz);
+void gauss(FLOAT *arreglo, FLOAT *arreglo_new, FLOAT amp, FLOAT sigma);
+void jeans(FLOAT *arreglo, FLOAT *arreglo_new, FLOAT rho, FLOAT amp, FLOAT sig, int n);
+void densidad(FLOAT *fase, FLOAT *rho);
+void potential(FLOAT *rho, FLOAT *Va, FLOAT *V_temp);
+void potfourier_real(FLOAT *rho, FLOAT *res);
+void acceleration(FLOAT *Va, FLOAT *aceleracion);
+void update(FLOAT * fase, FLOAT * azz, FLOAT * phase_temp);
 int ndx(int fila, int column);
 void printINFO(FLOAT * density, FILE * dens_file, FLOAT * azz, FILE * azz_file, FLOAT * potencial, FILE * pot_file, FLOAT * fase, FILE * fase_file);
 void printCONS();
@@ -69,27 +71,22 @@ int main(){
   FLOAT *pot_new;
   pot_new=malloc(sizeof(FLOAT)*Nx);
 
-  for(i=0;i<Nv;i++){
-    for(j=0;j<Nx;j++){
-      phase[ndx(i,j)]=gauss(L_min+j*delx, V_min+i*delv, 4.0, 0.08);
-      //phase[ndx(i,j)]=jeans(L_min+j*delx, V_min+i*delv, 5.0, 0.01, 0.5, 2);
-      phase_new[ndx(i,j)]=phase[ndx(i,j)];
 
-    }
-  }
+  gauss(phase, phase_new, 4.0, 0.08);
+  //jeans(phase, phase_new, 5.0, 0.01, 0.5, 2);
 
   for(k=0;k<T;k++){
 
-    dens=densidad(phase);
-    //pot_new=potential(dens);
+    densidad(phase, dens);
+    //potential(dens, pot_new);
     //pot_new=potfourier(dens);
-    pot_new=potfourier_real(dens);
-    acc=acceleration(pot_new);
+    potfourier_real(dens, pot_new);
+    acceleration(pot_new, acc);
     if(k%skip==0){
         printINFO(dens, dens_dat, acc, acc_dat, pot_new, pot_dat, phase, phase_dat);
     }
 
-    phase_new=update(phase, acc);
+    update(phase, acc, phase_new);
 
     for(i=0;i<Nv;i++){
       for(j=0;j<Nx;j++){
@@ -105,30 +102,37 @@ int main(){
   return 0;
 }
 
-FLOAT gauss(FLOAT pos, FLOAT vel, FLOAT amp, FLOAT sigma){
-  return amp*exp(-(pow(pos,2)+pow(vel,2))/sigma);
+void gauss(FLOAT *arreglo, FLOAT *arreglo_new, FLOAT amp, FLOAT sigma){
+  for(i=0;i<Nv;i++){
+    for(j=0;j<Nx;j++){
+      FLOAT pos=L_min+j*delx;
+      FLOAT vel=V_min+i*delv;
+      arreglo[ndx(i,j)]=amp*exp(-(pow(pos,2)+pow(vel,2))/sigma);
+      arreglo_new[ndx(i,j)]=arreglo[ndx(i,j)];
+    }
+  }
 }
-FLOAT jeans(FLOAT pos, FLOAT vel, FLOAT rho, FLOAT amp, FLOAT sig, int n){
+void jeans(FLOAT *arreglo, FLOAT *arreglo_new, FLOAT rho, FLOAT amp, FLOAT sig, int n){
   FLOAT k0 = 2.0*pi/L;
   FLOAT k = n*k0;
-  return rho/(pow(2*pi*sig*sig,0.5))*exp(-pow(vel,2)/(2*sig*sig))*(1+amp*cos(k*pos));
+  for(i=0;i<Nv;i++){
+    for(j=0;j<Nx;j++){
+      FLOAT pos=L_min+j*delx;
+      FLOAT vel=V_min+i*delv;
+      arreglo[ndx(i,j)]=rho/(pow(2*pi*sig*sig,0.5))*exp(-pow(vel,2)/(2*sig*sig))*(1+amp*cos(k*pos));
+      arreglo_new[ndx(i,j)]=arreglo[ndx(i,j)];
+    }
+  }
 }
-FLOAT * densidad(FLOAT *fase){
-  FLOAT * rho;
-  rho=malloc(sizeof(FLOAT)*Nx);
+void densidad(FLOAT *fase, FLOAT *rho){
   for(i=0;i<Nx;i++){
     rho[i]=0.0;
     for(j=0;j<Nv;j++){
       rho[i]+=fase[ndx(j,i)];
     }
   }
-  return rho;
 }
-FLOAT * potential(FLOAT *rho){
-  FLOAT *Va;
-  FLOAT *V_temp;
-  Va=malloc(sizeof(FLOAT)*Nx);
-  V_temp=malloc(sizeof(FLOAT)*Nx);
+void potential(FLOAT *rho, FLOAT *Va, FLOAT *V_temp){
   for(i=0;i<Nx;i++){
     V_temp[i]=0.0;
   }
@@ -142,50 +146,15 @@ FLOAT * potential(FLOAT *rho){
       V_temp[i]=Va[i];
     }
   }
-  return Va;
 }
-FLOAT * potfourier(FLOAT *rho){
+void potfourier_real(FLOAT *rho, FLOAT *res){
   FLOAT Kx;
   FLOAT kx;
-  FLOAT * res;
-  res=malloc(sizeof(FLOAT)*Nx);
-  fftw_complex *rho_in, *rho_out, *rho_fin;
-  fftw_plan rho_plan;
-  rho_in=malloc(sizeof(fftw_complex)*Nx);
-  rho_out=malloc(sizeof(fftw_complex)*Nx);
-  rho_fin=malloc(sizeof(fftw_complex)*Nx);
-
-  rho_plan = fftw_plan_dft_1d(Nx, rho_in, rho_out, FFTW_FORWARD, FFTW_ESTIMATE);
-  for(i=0;i<Nx;i++){
-    rho_in[i]=rho[i];
-  }
-  fftw_execute(rho_plan);
-  fftw_destroy_plan(rho_plan);
-
-  rho_plan = fftw_plan_dft_1d(Nx, rho_out, rho_fin, FFTW_BACKWARD, FFTW_ESTIMATE);
-  rho_out[0]=0.0;
-  for(i=1;i<Nx;i++){
-    kx=1/L*(FLOAT)i;
-    Kx=kx*sinc(0.5*kx*delx);
-    rho_out[i]=rho_out[i]/(-pow(Kx,2)*2*Nx);
-  }
-  fftw_execute(rho_plan);
-
-  for(i=0;i<Nx;i++){
-    res[i]=creal(rho_fin[i]/(2*Nx));
-  }
-  return res;
-}
-FLOAT * potfourier_real(FLOAT *rho){
-  FLOAT Kx;
-  FLOAT kx;
-  FLOAT * res;
   int ncx=(Nx/2+1);
   FLOAT *rho_in, *rho_fin;
   fftw_complex *rho_out;
   fftw_plan rho_plan;
 
-  res=malloc(sizeof(FLOAT)*Nx);
   rho_in=fftw_malloc(sizeof(FLOAT)*Nx);
   rho_out=fftw_malloc(sizeof(fftw_complex)*ncx);
   rho_fin=fftw_malloc(sizeof(FLOAT)*Nx);
@@ -201,32 +170,26 @@ FLOAT * potfourier_real(FLOAT *rho){
   for(i=1;i<Nx;i++){
     kx=1/L*(FLOAT)i;
     Kx=kx*sinc(0.5*kx*delx);
-    rho_out[i]=rho_out[i]/(-pow(Kx,2)*4*Nx);
+    rho_out[i]=rho_out[i]/(-pow(Kx,2)*pi*pow(2,0.5));
   }
   rho_plan = fftw_plan_dft_c2r_1d(Nx, rho_out, rho_fin, FFTW_ESTIMATE);
   fftw_execute(rho_plan);
   fftw_destroy_plan(rho_plan);
 
   for(i=0;i<Nx;i++){
-    res[i]=rho_fin[i]/(2*Nx);
+    res[i]=rho_fin[i]/(pi*pow(2,0.5)*Nx*500);
   }
-  return res;
+  fftw_free(rho_in); fftw_free(rho_out); fftw_free(rho_fin);
 }
-FLOAT * acceleration(FLOAT *Va){
-  FLOAT *aceleracion;
-  aceleracion=malloc(sizeof(FLOAT)*Nx);
+void acceleration(FLOAT *Va, FLOAT *aceleracion){
   aceleracion[0]=0; aceleracion[Nx-1]=0;
   for(i=1;i<Nx-1;i++){
     aceleracion[i]=-(Va[i+1]-Va[i-1])/(2.0*delx);
   }
-  return aceleracion;
 }
-FLOAT * update(FLOAT * fase, FLOAT * azz){
+void update(FLOAT * fase, FLOAT * azz, FLOAT * phase_temp){
   int i_v_new, j_x_new;
   FLOAT x, v, x_new, v_new;
-
-  FLOAT * phase_temp;
-  phase_temp = malloc(sizeof(FLOAT)*Nv*Nv);
   for(i=0;i<Nv;i++){
     for(j=0;j<Nx;j++){
       phase_temp[ndx(i,j)]=0.0;
@@ -239,8 +202,8 @@ FLOAT * update(FLOAT * fase, FLOAT * azz){
       x=L_min+j*delx;
       v_new=v+deltat*azz[j];
       x_new=x+deltat*v_new;
-      i_v_new= (int) round((v_new-V_min)/delv);
-      j_x_new= (int) round((x_new-L_min)/delx);
+      i_v_new= ((v_new-V_min)/delv);
+      j_x_new= ((x_new-L_min)/delx);
 
       if(i_v_new >= 0 && i_v_new < Nv){
         if(j_x_new < 0){
@@ -253,7 +216,6 @@ FLOAT * update(FLOAT * fase, FLOAT * azz){
       }
     }
   }
-  return phase_temp;
 }
 int ndx(int fila, int column){
   return fila*Nx+column;
